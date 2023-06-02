b0c6547f5ff243b0c3995e9d55dfa6ba69fd27a1
EliasEduardo07 <134114617+EliasEduardo07@users.noreply.github.com>
2023-06-01 19:16:07 -0600

    Update metricas.py
    Se agrego la funcion de metros y se modifico la distancia uclideana

--- metricas.py
+++ metricas.py
@@ -9,8 +9,9 @@ import folium
 from streamlit_folium import folium_static
 import polyline
 import streamlit as st
+from geopy.distance import geodesic
 
-API_KEY = "AIzaSyCRoRdEBeMZUfx_kSjmB-Dgezk2jYWh7bQ"
+API_KEY = "AIzaSyCRoRdEBeMZUfx_kSjmB-Dgezk2jYWh7bQ "
 gmaps = googlemaps.Client(key=API_KEY)
 
 def direccion_a_coordenadas(direccion):
@@ -24,24 +25,33 @@ def buscar_supermercados(latitud, longitud, radio):
     supermercados = gmaps.places_nearby(location=(latitud, longitud), radius=radio, type='supermarket')['results']
     return supermercados
 
-def calcular_distancia_manhattan(coordenadas1, coordenadas2):
-    latitud1, longitud1 = coordenadas1
-    latitud2, longitud2 = coordenadas2
-    return abs(latitud1 - latitud2) + abs(longitud1 - longitud2)
- 
 def calcular_distancia_euclidiana(coordenadas1, coordenadas2):
-    latitud1, longitud1 = coordenadas1
-    latitud2, longitud2 = coordenadas2
-    return ((latitud1 - latitud2) ** 2 + (longitud1 - longitud2) ** 2) ** 0.5
+    return geodesic(coordenadas1, coordenadas2).meters
 
 def calcular_ruta(latitud_origen, longitud_origen, latitud_destino, longitud_destino):
-    directions_result = gmaps.directions((latitud_origen, longitud_origen),
-                                         (latitud_destino, longitud_destino),
-                                         mode="driving")
+    directions_result = gmaps.directions(
+        (latitud_origen, longitud_origen),
+        (latitud_destino, longitud_destino),
+        mode="transit",
+        units="metric"
+    )
     return directions_result
 
+def obtener_distancia_ruta(directions_result):
+    total_distance = 0
+    # The route is the first element of the response
+    route = directions_result[0]
+    # A route contains legs (steps from start to end)
+    for leg in route['legs']:
+        # A leg contains steps
+        for step in leg['steps']:
+            # Each step has a distance, which is a dict with 'text' (human-readable) and 'value' (meters)
+            total_distance += step['distance']['value']
+
+    return total_distance
+
 def app_metricas():
-    st.title("Metricas de distancia")
+    st.title("Métricas de distancia")
 
     ubicacion_usuario = st.text_input("Por favor ingresa tu ubicación (ejemplo: 'Ciudad de México'):")
 
@@ -50,7 +60,7 @@ def app_metricas():
 
         if latitud_actual and longitud_actual:
             st.write("Tu ubicación actual: {}, {}".format(latitud_actual, longitud_actual))
-            radio_busqueda = st.number_input("Ingrese             radio de búsqueda en metros:", min_value=100)
+            radio_busqueda = st.number_input("Ingresa el radio de búsqueda en metros:", min_value=100)
 
             supermercados_cercanos = buscar_supermercados(latitud_actual, longitud_actual, radio_busqueda)
             supermercados_y_distancias = []
@@ -62,10 +72,20 @@ def app_metricas():
                 latitud_supermercado = coordenadas_supermercado['lat']
                 longitud_supermercado = coordenadas_supermercado['lng']
 
-                distancia_manhattan = calcular_distancia_manhattan((latitud_actual, longitud_actual), (latitud_supermercado, longitud_supermercado))
-                distancia_euclidiana = calcular_distancia_euclidiana((latitud_actual, longitud_actual), (latitud_supermercado, longitud_supermercado))
+                distancia_euclidiana = calcular_distancia_euclidiana(
+                    (latitud_actual, longitud_actual),
+                    (latitud_supermercado, longitud_supermercado)
+                )
 
-                supermercados_y_distancias.append((nombre, distancia_manhattan, distancia_euclidiana, latitud_supermercado, longitud_supermercado))
+                ruta = calcular_ruta(
+                    latitud_actual, longitud_actual, latitud_supermercado, longitud_supermercado
+                )
+
+                distancia_ruta = obtener_distancia_ruta(ruta)
+
+                supermercados_y_distancias.append((
+                    nombre, distancia_ruta, distancia_euclidiana, latitud_supermercado, longitud_supermercado
+                ))
 
             supermercados_y_distancias.sort(key=lambda x: x[2])
 
@@ -73,10 +93,10 @@ def app_metricas():
             opciones_supermercados = [nombre for nombre, _, _, _, _ in supermercados_y_distancias]
             supermercado_seleccionado = st.selectbox("Seleccione un supermercado:", opciones_supermercados)
 
-            for nombre, distancia_manhattan, distancia_euclidiana, latitud_supermercado, longitud_supermercado in supermercados_y_distancias:
+            for nombre, distancia_ruta, distancia_euclidiana, latitud_supermercado, longitud_supermercado in supermercados_y_distancias:
                 if nombre == supermercado_seleccionado:
                     st.write("Nombre: ", nombre)
-                    st.write("Distancia Manhattan: ", distancia_manhattan)
+                    st.write("Distancia de ruta: ", distancia_ruta)
                     st.write("Distancia Euclidiana: ", distancia_euclidiana)
                     break
 
@@ -89,11 +109,16 @@ def app_metricas():
             if st.button("Calcular ruta"):
                 for nombre, _, _, latitud_supermercado, longitud_supermercado in supermercados_y_distancias:
                     if nombre == supermercado_seleccionado:
-                        ruta = calcular_ruta(latitud_actual, longitud_actual, latitud_supermercado, longitud_supermercado)
+                        ruta = calcular_ruta(
+                            latitud_actual, longitud_actual, latitud_supermercado, longitud_supermercado
+                        )
                         break
                 if ruta:
                     ruta_mapa = folium.Map(location=[latitud_actual, longitud_actual], zoom_start=13)
-                    folium.PolyLine([(step['start_location']['lat'], step['start_location']['lng']) for step in ruta[0]['legs'][0]['steps']], color="green", weight=2.5, opacity=1).add_to(ruta_mapa)
+                    folium.PolyLine(
+                        [(step['start_location']['lat'], step['start_location']['lng']) for step in ruta[0]['legs'][0]['steps']],
+                        color="green", weight=2.5, opacity=1
+                    ).add_to(ruta_mapa)
                     folium_static(ruta_mapa)
                 else:
                     st.write("No se pudo calcular la ruta.")
